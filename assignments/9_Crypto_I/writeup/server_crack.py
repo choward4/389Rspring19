71ddf8357136e8c8f5fa2903277b3dbc1a106ccb
choward4 <chrishoward1337@yahoo.com>
2019-04-26 15:08:48 -0400

    please work

--- assignments/9_Crypto_I/writeup/server_crack.py
+++ assignments/9_Crypto_I/writeup/server_crack.py
@@ -6,17 +6,54 @@ import socket
 import time
 
 def server_crack():
-    hashes = # open and read hashes.txt
-    passwords = # open and read passwords.txt
+    f = open("hashes.txt", 'r')
+    hashes = f.read().split('\n')
+    hashes.pop()
+    f.close()
+
+    f = open("passwords.txt", 'r')
+    passwords = f.read().split('\n')
+    passwords.pop()
+    f.close()
+    
     characters = string.ascii_lowercase
     server_ip = '134.209.128.58'
     server_port = 1337
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((server_ip, server_port))
-    data = s.recv(1024)
-    # parse data
+    
+    times = 0
+    while times < 3:
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print(times)
+        # s.connect((server_ip, server_port))
+        data = s.recv(1024)
+        print("data:", data)
+        data = data.decode().split('\n')
+        crackHash = data[2]
+        answer = ''
+        for c in characters:
+            for p in passwords:
+                inputStr = c + p
+                to_hash = inputStr.encode('utf-8')
+                cpHash = hashlib.sha256(to_hash)
+                hex_dig = cpHash.hexdigest()
+
+                if hex_dig == crackHash:
+                    print("hex dig matched hash")
+                    answer = to_hash
+                   
+        
+        s.send(answer)
+        s.send("\n".encode("utf-8"))
+        print(answer)
+        times += 1
+    
     # crack 3 times
+    flag = s.recv(1024)
+    flag = flag.decode()
+    print(flag)
 
 if __name__ == "__main__":
     server_crack()
